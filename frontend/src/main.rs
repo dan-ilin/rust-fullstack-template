10d676f79c329ff40290ec6673fb1eb35101e864
Dan Ilin <dan.ilin@me.com>
2023-10-11 11:53:19 -0400

    Frontend hello world

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
+use yew::prelude::*;
 
+fn main() {
+    yew::Renderer::<App>::new().render();
+}
+
+#[function_component]
+fn App() -> Html {
+    html! {
+        <p>{"hello world"}</p>
+    }
 }
